f08d7e36a66e2f3b7761e6c5ac6f2489a67553a5
Negin <negin.namiranian@gmail.com>
2018-02-03 13:09:37 -0800

    Required files added

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Login from './components/Login';
+
+ReactDOM.render(<Router>
+	<div>
+		<Route exact path="/" component={App} />
+		<Route exact path="/login" component={Login} />
+	</div>
+</Router>, document.getElementById('root'));
+registerServiceWorker();
+
+
